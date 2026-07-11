4b7223d8a4f6df04988198d8f87a26dbe3589382
Zsolt Harmath <harmathzs@gmail.com>
2026-07-11 09:57:46 +0200

    Add product interest display and wire to opportunity component

--- force-app/main/default/lwc/leadToOpportunityProductMapper/leadToOpportunityProductMapper.js
+++ force-app/main/default/lwc/leadToOpportunityProductMapper/leadToOpportunityProductMapper.js
@@ -4,10 +4,16 @@ import { getRecord } from 'lightning/uiRecordApi';
 export default class LeadToOpportunityProductMapper extends LightningElement {
   @api recordId;
 
-  @wire(getRecord, { recordId: '$recordId', fields: ['Opportunity.Id', 'Opportunity.Name'] })
+  @wire(getRecord, { recordId: '$recordId',
+    fields: ['Opportunity.Id', 'Opportunity.Name', 'Opportunity.Converted_Lead__c', 'Opportunity.Converted_Lead__r.ProductInterest__c'] })
   opportunity;
 
   get opportunityName() {
+    console.log('opportunity.data.fields', this.opportunity.data ? this.opportunity.data.fields : "no oppy data yet")
     return this.opportunity.data ? this.opportunity.data.fields.Name.value : 'Loading...';
+  }
+
+  get productInterest() {
+    return this.opportunity.data ? this.opportunity.data.fields.Converted_Lead__r.value.fields.ProductInterest__c.value : 'Loading...'
   }
 }
