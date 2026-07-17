de4c7020b64d065d594cd6babc5fa352ac59d9f1
Zsolt Harmath <harmathzs@gmail.com>
2026-07-17 13:34:27 +0200

    Add contract display section to contract PDF creator

--- force-app/main/default/lwc/contractPdfCreator/contractPdfCreator.js
+++ force-app/main/default/lwc/contractPdfCreator/contractPdfCreator.js
@@ -3,5 +3,19 @@ import { LightningElement, api } from 'lwc';
 import queryContract from '@salesforce/apex/ContractPDFcontroller.queryContract'
 
 export default class ContractPdfCreator extends LightningElement {
-  @api recordId;
+  contractId;
+  @api get recordId() {
+    return this.contractId
+  }
+  set recordId(value) {
+    this.contractId = value
+
+    this.loadData().catch(console.warn)
+  }
+
+  contract;
+  async loadData() {
+    this.contract = await queryContract({contractId: this.contractId})
+    console.log('contract', this.contract)
+  }
 }
