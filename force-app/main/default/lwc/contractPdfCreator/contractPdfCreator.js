aa392143a28414ac21e2475647e6a62c977a6971
Zsolt Harmath <harmathzs@gmail.com>
2026-07-17 13:27:49 +0200

    Add AuraEnabled annotation to queryContract method in ContractPDFcontroller and import queryContract in contractPdfCreator.js

--- force-app/main/default/lwc/contractPdfCreator/contractPdfCreator.js
+++ force-app/main/default/lwc/contractPdfCreator/contractPdfCreator.js
@@ -1,5 +1,7 @@
 import { LightningElement, api } from 'lwc';
 
+import queryContract from '@salesforce/apex/ContractPDFcontroller.queryContract'
+
 export default class ContractPdfCreator extends LightningElement {
   @api recordId;
 }
